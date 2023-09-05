84bb6e6e507b178d0a730a9d6cc743d5e3366d56
emaan-arshad <142867477+emaan-arshad@users.noreply.github.com>
2023-09-05 22:53:08 +0500

    Update ex04.c

--- lab03/ex04.c
+++ lab03/ex04.c
@@ -3,10 +3,10 @@
 int main() {
         int op=0;
         int pcount=0;
-        printf("program to find original price: \n");
+        printf("program to find sale price: \n");
         printf("original price: ");
         scanf("%d",&op);
-        printf("percentdiscount: ");
+        printf("percent discount: ");
         scanf("%d",&pcount);
         int salep= op-(pcount*op/100);
         printf("sale price is %d",salep);
