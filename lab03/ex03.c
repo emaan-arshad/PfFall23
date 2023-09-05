2e10e117bf9bbab65f1f2bc5a808e66c2f71f221
emaan-arshad <142867477+emaan-arshad@users.noreply.github.com>
2023-09-05 22:51:57 +0500

    Update ex03.c

--- lab03/ex03.c
+++ lab03/ex03.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <math.h>
 int main() {
-        int base=0;
-        int height=0;
+        float base=0;
+        float height=0;
         printf("program to find hypotneuse:\n");
         printf("base? ");
-        scanf("%d",&base);
+        scanf("%f",&base);
         printf("height? ");
-        scanf("%d",&height);
-        int hypo = sqrt(base * base   +  height * height);
-        printf("%d",hypo);
+        scanf("%f",&height);
+        float hypo = sqrt(base * base   +  height * height);
+        printf("hypotenuse is %f",hypo);
         return 1;
 }//end
