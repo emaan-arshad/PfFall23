94790c74bb85fcbd54f636c5b154a7bc16fe474a
emaan-arshad <142867477+emaan-arshad@users.noreply.github.com>
2023-09-12 22:01:26 +0500

    Update Q3.c

--- lab04/Q3.c
+++ lab04/Q3.c
@@ -1,3 +1,9 @@
+//programmer: Emaan Arshad (23I-2560)
+//date: 9/12/23
+//description:. Write a program that asks user for a number n, then asks for n more integers and prints the sum of their squares.
+
+
+
 #include <stdio.h>
 #include <math.h>
 
@@ -15,7 +21,7 @@ int main() {
             scanf("%d",&a);
             ans = ans + (a * a);
             i = i + 1;
-     }
+     }//end while(i<=N)
      printf("%d",ans);      
      
           
