6c46f5fbd8208243b181a35b1de1ecbbc92704cf
emaan-arshad <142867477+emaan-arshad@users.noreply.github.com>
2023-09-20 23:43:54 +0500

    Update q3.C

--- LAB 05/q3.C
+++ LAB 05/q3.C
@@ -1,50 +1,33 @@
+
 #include <stdio.h>
 
 int main()
 {
      char ans;
-    printf("Are u sure u wanto delete this orogram, input Y or y for yes else input N or n for no : ");
+    printf("Are u sure u wanto delete this Program, input Y or y for yes else input N or n for no : ");
     scanf("%c",&ans);
-    if (ans== "Y" || ans=="y") {
-        ans=1;
-    }
-    if (ans== "N" || ans== "n"){
-        ans =2;
-    }
+    //if (ans= "Y" || ans="y") {
+       // ans=1;
+    //}
+    //if (ans= "N" || ans= "n"){
+      //  ans =2;
+   // }
     
     switch (ans) {
-        case 1:
+        case 'Y':
             printf("Deleted successfully");
             break;
-        case 2:
+        case 'N':
         printf("Delete cancelled");
-        
-    }
-    
-    return 0;
-}
-#include <stdio.h>
-
-int main()
-{
-     char ans;
-    printf("Are u sure u wanto delete this orogram, input Y or y for yes else input N or n for no : ");
-    scanf("%c",&ans);
-    if (ans== "Y" || ans=="y") {
-        ans=1;
-    }
-    if (ans== "N" || ans== "n"){
-        ans =2;
-    }
-    
-    switch (ans) {
-        case 1:
-            printf("Deleted successfully");
-            break;
-        case 2:
+        break;
+        case 'n':
         printf("Delete cancelled");
-        
+        break;
+        case 'y':
+        printf("Deleted succesfully");
+        break;
+        default :
+        printf("are u sure u are entering the right letter??");
     }
-    
     return 0;
 }
