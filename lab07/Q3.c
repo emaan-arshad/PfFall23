1708f70d719caf616457d15e61f4747c9802266d
emaan-arshad <142867477+emaan-arshad@users.noreply.github.com>
2023-10-18 23:01:21 +0500

    Create Q3.c

---
+++ lab07/Q3.c
@@ -0,0 +1,25 @@
+//programmer:Emaan Arshad
+//date:10/10/23
+//desc: sum  of digits of a number
+
+
+#include <stdio.h>
+
+int main (){
+int N, digit;
+  int sum=0;
+
+  printf ("Enter a number");
+  scanf ("%d" ,&num);
+
+while (N) {
+digit = N % 10;
+N /= 10;
+  sum += sum;
+}
+  
+
+printf ("The sum of digits  is %d" ,sum);
+
+return 0;
+}
