f35d4fa6ef7c05fc666449dbe04be625d880bb4b
emaan-arshad <142867477+emaan-arshad@users.noreply.github.com>
2023-11-03 00:52:55 +0500

    Update Q7.c

--- Assignment 02/Q7/Q7.c
+++ Assignment 02/Q7/Q7.c
@@ -64,7 +64,7 @@ for(int i=0;i<10;i++) {
   
     for(int i=0;i<10;i++) {
       for(int j=i +1;j<10;j++) {
-         if( arr[i][0]==arr[j][0] ) {
+        
               if(arr[i][1]<arr[j][1]) {
                   x=arr[i][1];
                   arr[i][1]=arr[j][1];
@@ -80,7 +80,7 @@ for(int i=0;i<10;i++) {
       }
       
       
-    }
+    
           
           
           
