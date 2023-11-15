05629c3b0ea1ad4d8f0f055e131bfb86d69bebc9
emaan-arshad <142867477+emaan-arshad@users.noreply.github.com>
2023-11-15 23:14:56 +0500

    Create Q3.c

---
+++ lab 10/Q3.c
@@ -0,0 +1,59 @@
+//programmer:Emaan Arshad
+//date:11/15/23
+  //desc:to fill array with prime numbers using dynamic memeory allocation
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+void prime(int ** ptr,int rows,int cols);
+ bool isPrime(int n);
+int main()
+{
+    int **ptr;
+    int rows,cols;
+    printf("enter no of rows and cols: \n");
+    scanf("%d%d",&rows,&cols);
+    ptr=(int**)malloc(rows * sizeof(int*));
+    
+    for(int i=0;i<rows;i++) {
+       *(ptr+i)=(int*)malloc(cols * sizeof(int));
+    }
+
+
+
+
+prime(ptr,rows,cols);
+for(int i =0;i<rows;i++) {
+    for(int j=0;j<cols;j++) {
+        printf("%d ", *(*(ptr+i)+j));
+    }
+    printf("\n");
+}
+    return 0;
+}
+void prime(int ** ptr,int rows,int cols) {
+     int num = 2;
+     for(int i=0;i<rows;i++) {
+        for(int j=0;j<cols;j=j) {
+        if (isPrime(num)) {
+            *(*(ptr+i)+j) = num;
+            j++;  
+        }
+        num++;
+    }
+    }
+
+}  
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
