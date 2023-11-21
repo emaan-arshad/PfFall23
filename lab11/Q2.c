206f0c35b9fceb53cd4fc39722facb123f518506
emaan-arshad <142867477+emaan-arshad@users.noreply.github.com>
2023-11-21 10:27:59 +0500

    Update Q2.c

--- lab11/Q2.c
+++ lab11/Q2.c
@@ -1,6 +1,11 @@
+//programmer:Emaan Arshad
+//date:21/11/23
+//desc:struct of engine parts
+
 
 #include <stdio.h>
 #include <string.h>
+//declaring engine array
   struct ENGINE{
         char mat[100];
         int quan;
@@ -18,6 +23,7 @@ int n;
 printf("enter no of engines :\n");
 struct ENGINE parts[100]; 
 scanf("%d",&n);
+//taking n inputs for serialno,quan,year,mat
 while(i<n)
 {
     printf("\nengine no:%d",i+1);
@@ -32,17 +38,18 @@ scanf("%d",&parts[i].year);
 
 ++i;
 }
+
 printf("The engine parts which have serial number from BB1-CC6 have the following data:  ");
    
     for(int i=0;i<n;i++) {
-        int ret=strcmp(parts[i].s_no,"AA0");
+        int ret=strcmp(parts[i].s_no,"BB1");//checking for boundaries by string comparision 
         int ret1=strcmp(parts[i].s_no,"CC6");
-        if(ret>=0 && ret1<=0)  {
+        if(ret>=0 && ret1<=0)  {//printing all the info of matched serial nos
         printf("%s",parts[i].s_no);
         printf("Manufacture year: %d\n",parts[i].year);
         printf("Material: %s\n",parts[i].mat);
         printf("Quantity: %d\n\n",parts[i].quan);
-        }
-     }
+        }//end if
+     }//end for
     return 0;
-}
+}//endmain
